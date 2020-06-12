aa57b966e6119f94fa0849dfc1bdb6b0c2aae412
Will Harris <will.t.harris@protonmail.com>
2020-06-11 22:01:33 -0700

    feat: define basic express app

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const session = require("express-session");
+const mongoose = require("mongoose");
+const MongoStore = require("connect-mongo")(session);
+const path = require("path");
+const bodyParser = require("body-parser");
+const routes = require("./routes/index");
+
+const app = express();
+
+app.set("views", path.join(__dirname, "views"));
+
+app.set("view engine", "pug");
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// app.use(expressValidator());
+
+app.use((req, res, next) => {
+	res.locals.currentPath = req.path;
+	next();
+});
+
+app.use("/", routes);
+
+module.exports = app;
