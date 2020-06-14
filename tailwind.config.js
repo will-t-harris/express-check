032fb909fb11c2946be9c2d549202096788970cb
Will Harris <will.t.harris@protonmail.com>
2020-06-14 12:29:06 -0700

    config: use different tw separator for pug

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
 module.exports = {
-  purge: [],
-  theme: {
-    extend: {},
-  },
-  variants: {},
-  plugins: [],
-}
+	purge: [],
+	separator: "_",
+	theme: {
+		extend: {},
+	},
+	variants: {},
+	plugins: [],
+};
