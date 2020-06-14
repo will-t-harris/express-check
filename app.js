a8cd80c4846eeeab3995a7bf2b5ea62ad1128204
Will Harris <will.t.harris@protonmail.com>
2020-06-14 12:29:16 -0700

    refactor: move static use to top

--- app.js
+++ app.js
@@ -10,11 +10,11 @@ const routes = require("./routes/index");
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, "public")));
+
 app.set("views", path.join(__dirname, "views"));
 
 app.set("view engine", "pug");
-
-app.use(express.static(path.join(__dirname, "public")));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
