0e7150aae59cac7457e085aaa63e6466f1a9b3ee
Will Harris <will.t.harris@protonmail.com>
2020-06-17 11:41:39 -0700

    feat: add priority limit validation to schema

--- backend/models/Todo.js
+++ backend/models/Todo.js
@@ -4,12 +4,21 @@ const Schema = mongoose.Schema;
 
 const todoSchema = new Schema(
 	{
-		todoContent: { type: String, required: true },
-		todoPriority: { type: Number, required: true },
+		todoContent: { type: String, required: true, trim: true },
+		todoPriority: {
+			type: Number,
+			required: true,
+			trim: true,
+			validate: [priorityLimit, "Priority must be between 0 and 4"],
+		},
 		todoDate: { type: Date, required: true },
 	},
 	{ timestamps: true }
 );
+
+function priorityLimit(value) {
+	return value > 0 && value <= 4;
+}
 
 const Todo = mongoose.model("Todo", todoSchema);
 
