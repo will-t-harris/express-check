f5b43ccd8b1276bdf23680e50672e13cb555d6c0
Will Harris <will.t.harris@protonmail.com>
2020-06-17 12:10:23 -0700

    feat: add react-router-dom & add routes to app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import { TodoList } from "./components/TodoList";
+import { EditTodo } from "./components/EditTodo";
+import { CreateTodo } from "./components/CreateTodo";
 
 function App() {
 	return (
-		<div>
-			<p>App.js</p>
-		</div>
+		<Router>
+			<div>
+				<Route path="/" exact component={TodoList} />
+				<Route path="/edit/:id" component={EditTodo} />
+				<Route path="/create" component={CreateTodo} />
+			</div>
+		</Router>
 	);
 }
 
