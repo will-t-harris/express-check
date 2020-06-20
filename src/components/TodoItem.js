db4c99b54ef68518be3dd2ad479544df29038c50
Will Harris <will.t.harris@protonmail.com>
2020-06-20 14:00:00 -0700

    refactor: abandon html table setup

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,11 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-export const TodoItem = (props) => {
+export const TodoItem = ({ todoContent, todoPriority, todoDate, id }) => {
 	return (
-		<tr>
-			<td>{props.todoContent}</td>
-			<td>{props.todoPriority}</td>
-			<td>{props.todoDate}</td>
-		</tr>
+		<div>
+			<ul className="flex">
+				<li className="">{todoContent}</li>
+				<li className="text-center">{todoPriority}</li>
+				<li className="">{todoDate}</li>
+			</ul>
+			<Link to={`/edit/${id}`}>Edit Todo</Link>
+		</div>
 	);
 };
