e98bc60bd342847a811d27c62cf2ec2de13a043c
Will Harris <will.t.harris@protonmail.com>
2020-06-20 13:59:37 -0700

    style: tweak sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -3,9 +3,13 @@ import { Link } from "react-router-dom";
 
 export const Sidebar = () => {
 	return (
-		<div className="float-left h-screen bg-purple-500 w-1/6">
-			<Link to="/">Home</Link>
-			<Link to="/create">Add Todo</Link>
+		<div className="float-left flex flex-col h-screen bg-gray-800 text-white text-xl font-bold text-center w-1/6">
+			<Link className="py-8 hover:underline" to="/">
+				Home
+			</Link>
+			<Link className="py-8 hover:underline" to="/create">
+				Add Todo
+			</Link>
 		</div>
 	);
 };
