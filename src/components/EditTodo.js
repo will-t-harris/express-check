ebb544a790a210e5ed1ff26237a72aaa4c1dd4dc
Will Harris <will.t.harris@protonmail.com>
2020-06-20 21:12:44 -0700

    feat: add edittodo component

--- src/components/EditTodo.js
+++ src/components/EditTodo.js
@@ -1,9 +1,97 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import DatePicker from "react-datepicker";
+import axios from "axios";
 
-export const EditTodo = () => {
+import { Sidebar } from "./Sidebar";
+
+export const EditTodo = (props) => {
+	const [todos, setTodos] = useState(() => {
+		return { todoContent: "", todoPriority: 4, todoDate: new Date() };
+	});
+	const todoId = props.match.params.id;
+
+	useEffect(() => {
+		axios
+			.get(`http://localhost:5000/todos/${todoId}`)
+			.then((response) => {
+				console.log(response);
+				setTodos({ ...response.data });
+			})
+			.catch((err) => console.error(err));
+	}, [todoId]);
+
+	const onChangeTodoContent = (event) => {
+		setTodos({ ...todos, todoContent: event.target.value });
+	};
+
+	const onChangeTodoPriority = (event) => {
+		setTodos({ ...todos, todoPriority: event.target.value });
+	};
+
+	const onChangeTodoDate = (todoDate) => {
+		setTodos({ ...todos, todoDate });
+	};
+
+	const onSubmit = (event) => {
+		event.preventDefault();
+
+		const todo = {
+			todoContent: todos.todoContent,
+			todoPriority: todos.todoPriority,
+			todoDate: todos.todoDate,
+		};
+
+		axios
+			.post(`http://localhost:5000/todos/edit/${todoId}`, todo)
+			.then((response) => console.log(response.data))
+			.then(() => {
+				window.location = "/";
+			})
+			.catch((err) => console.error(err));
+	};
+
 	return (
 		<div>
-			<p>You are on the Edit Todo component</p>
+			<Sidebar />
+			<h3 className="text-3xl text-indigo-800">Create New Todo</h3>
+			<form onSubmit={onSubmit}>
+				<div className="mb-2 text-center">
+					<label>Todo Content: </label>
+					<input
+						className="border border-indigo-600"
+						type="text"
+						required
+						value={todos.todoContent}
+						onChange={onChangeTodoContent}
+					/>
+				</div>
+				<div className="mb-2 text-center">
+					<label>Todo Priority: </label>
+					<input
+						className="border border-indigo-600"
+						type="number"
+						min={0}
+						max={4}
+						value={todos.todoPriority}
+						onChange={onChangeTodoPriority}
+					/>
+				</div>
+				<div className="mb-2 text-center">
+					<label>Todo Due Date: </label>
+
+					<DatePicker
+						selected={todos.todoDate ? new Date(todos.todoDate) : null}
+						onChange={onChangeTodoDate}
+					/>
+				</div>
+				<div className="text-center">
+					<input
+						className="px-4 py-2 rounded bg-indigo-200 hover:bg-indigo-400 cursor-pointer"
+						type="submit"
+						value="Update Todo"
+					/>
+				</div>
+			</form>
 		</div>
 	);
 };
