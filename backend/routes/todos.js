3b4bca96f0aba27cde77c14632bea08891e208d9
Will Harris <will.t.harris@protonmail.com>
2020-06-20 21:12:29 -0700

    edit: change update route to edit

--- backend/routes/todos.js
+++ backend/routes/todos.js
@@ -36,7 +36,7 @@ router.route("/:id").delete((req, res) => {
 		.catch((err) => res.status(400).json(`Error: ${err}`));
 });
 
-router.route("/update/:id").post((req, res) => {
+router.route("/edit/:id").post((req, res) => {
 	Todo.findById(req.params.id).then((todo) => {
 		todo.todoContent = req.body.todoContent;
 		todo.todoPriority = Number(req.body.todoPriority);
