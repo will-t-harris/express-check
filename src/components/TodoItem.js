d2e63352a58da8eca6afedd26be9da7aa6d8f24c
Will Harris <will.t.harris@protonmail.com>
2020-06-20 17:02:53 -0700

    refactor: todo item component markup

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,15 +1,31 @@
 import React from "react";
+import moment from "moment";
 import { Link } from "react-router-dom";
 
-export const TodoItem = ({ todoContent, todoPriority, todoDate, id }) => {
+export const TodoItem = ({
+	todoContent,
+	todoPriority,
+	todoDate,
+	id,
+	deleteTodo,
+}) => {
+	let date = moment(todoDate);
 	return (
-		<div>
-			<ul className="flex">
-				<li className="">{todoContent}</li>
-				<li className="text-center">{todoPriority}</li>
-				<li className="">{todoDate}</li>
-			</ul>
-			<Link to={`/edit/${id}`}>Edit Todo</Link>
+		<div className="pl-8 pb-8 bg-gray-700 text-white">
+			<p className="float-left">{todoContent}</p>
+			<p className="">Priority: {todoPriority}</p>
+			<p className="">{date.format("MMM D")}</p>
+			<Link className="text-green-600 font-semibold" to={`/edit/${id}`}>
+				Edit Todo
+			</Link>{" "}
+			|{" "}
+			<button
+				className="text-red-600 font-semibold"
+				onClick={() => deleteTodo(id)}
+			>
+				Delete Todo
+			</button>
+			<hr className="border border-gray-900" />
 		</div>
 	);
 };
