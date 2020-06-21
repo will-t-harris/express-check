449990e7752ec466459746fb81d934e83d370043
Will Harris <will.t.harris@protonmail.com>
2020-06-20 17:03:37 -0700

    feat: add basic setup for todolist component

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,9 +1,48 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+import { Sidebar } from "./Sidebar";
+import { TodoItem } from "./TodoItem";
 
 export const TodoList = () => {
+	const [todos, setTodos] = useState([]);
+
+	useEffect(() => {
+		axios
+			.get("http://localhost:5000/todos")
+			.then((response) => {
+				setTodos(response.data);
+			})
+			.catch((err) => console.error(err));
+	}, []);
+
+	const deleteTodo = (id) => {
+		axios
+			.delete(`http://localhost:5000/todos/${id}`)
+			.then((response) => console.log(response.data))
+			.catch((err) => console.error(err));
+
+		const filteredTodos = todos.filter((todo) => todo._id !== id);
+		setTodos(filteredTodos);
+	};
+
+	const listTodos = () =>
+		todos.map((todo) => (
+			<TodoItem
+				key={todo._id}
+				id={todo._id}
+				deleteTodo={deleteTodo}
+				todoContent={todo.todoContent}
+				todoPriority={todo.todoPriority}
+				todoDate={todo.todoDate}
+			/>
+		));
+
 	return (
 		<div>
-			<p>You are on the Todo List component</p>
+			<Sidebar />
+			<h2 className="text-xl font-extrabold text-center py-8">Current Todos</h2>
+			<div className="flex flex-col">{listTodos()}</div>
 		</div>
 	);
 };
