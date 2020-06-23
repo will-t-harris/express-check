2932f30c7c8928235dfcb4a0e971a13bf7461140
Will Harris <will.t.harris@protonmail.com>
2020-06-22 21:39:12 -0700

    feat: sort todos by  date in router

--- backend/routes/todos.js
+++ backend/routes/todos.js
@@ -3,6 +3,7 @@ const Todo = require("../models/Todo");
 
 router.route("/").get((req, res) => {
 	Todo.find()
+		.then((todos) => todos.sort((a, b) => a.todoDate - b.todoDate))
 		.then((todos) => res.json(todos))
 		.catch((err) => res.status(400).json(`Error: ${err}`));
 });
