3bb6d10e2540a5b5727f3bd5e03a5752806085ff
Will Harris <will.t.harris@protonmail.com>
2020-07-15 16:01:21 -0700

    chore: install connect-flash

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const flash = require("connect-flash");
 
 require("dotenv").config();
 
@@ -9,6 +10,8 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+app.use(flash());
 
 const uri = process.env.ATLAS_URI;
 
