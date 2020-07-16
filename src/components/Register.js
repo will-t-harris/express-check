f25ecf0089abba32acbaf84c133b60c31e0e6889
Will Harris <will.t.harris@protonmail.com>
2020-07-15 17:32:01 -0700

    feat: work on register component

--- src/components/Register.js
+++ src/components/Register.js
@@ -4,7 +4,12 @@ import axios from "axios";
 import { Header } from "./Header";
 
 export const Register = (props) => {
-	const [user, setUser] = useState({ email: "", name: "" });
+	const [user, setUser] = useState({
+		email: "",
+		name: "",
+		password: "",
+		passwordConfirm: "",
+	});
 
 	const onSubmit = (event) => {
 		event.preventDefault();
@@ -12,9 +17,11 @@ export const Register = (props) => {
 		const newUser = {
 			email: user.email,
 			name: user.name,
+			password: user.password,
+			passwordConfirm: user.passwordConfirm,
 		};
 		axios
-			.post("http://localhost:5000/users/add", newUser)
+			.post("http://localhost:5000/users/register", newUser)
 			.then((res) => console.log(res.data));
 	};
 
@@ -28,7 +35,6 @@ export const Register = (props) => {
 					className="bg-purple-100"
 					type="text"
 					name="name"
-					required
 					onChange={(event) => setUser({ ...user, name: event.target.value })}
 				/>
 				<label htmlFor="email">Email</label>
@@ -36,22 +42,27 @@ export const Register = (props) => {
 					className="bg-purple-100"
 					type="email"
 					name="email"
-					required
 					onChange={(event) => setUser({ ...user, email: event.target.value })}
 				/>
 				<label htmlFor="password">Password</label>
 				<input
 					className="bg-purple-100"
-					htmlFor="password"
 					type="password"
 					name="password"
+					minLength={8}
+					onChange={(event) =>
+						setUser({ ...user, password: event.target.value })
+					}
 				/>
-				<label htmlFor="password-confirm">Confirm Password</label>
+				<label htmlFor="passwordConfirm">Confirm Password</label>
 				<input
 					className="bg-purple-100"
-					htmlFor="password-confirm"
 					type="password"
-					name="password-confirm"
+					name="passwordConfirm"
+					minLength={8}
+					onChange={(event) =>
+						setUser({ ...user, passwordConfirm: event.target.value })
+					}
 				/>
 				<input type="submit" value="Register &#10148;" />
 			</form>
