7fa7c3838ba5a9fb6eb73c83e1a0c3103582f03d
Will Harris <will.t.harris@protonmail.com>
2020-07-15 17:31:39 -0700

    feat: work on register middleware

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const User = require("../models/User");
+const { check, validationResult } = require("express-validator");
 
 router.route("/").get((req, res) => {
 	User.find()
@@ -7,16 +8,48 @@ router.route("/").get((req, res) => {
 		.catch((err) => res.status(400).json(`Error: ${err}`));
 });
 
-router.route("/add").post((req, res) => {
-	const email = req.body.email;
-	const name = req.body.name;
-
-	const newUser = new User({ email, name });
-
-	newUser
-		.save()
-		.then(() => res.json("User added"))
-		.catch((err) => res.status(400).json(`Error: ${err}`));
-});
+router.post(
+	"/register",
+	[
+		check("email", "That is not a valid email")
+			.notEmpty()
+			.isEmail()
+			.normalizeEmail({
+				gmail_remove_dots: false,
+				gmail_remove_subaddress: false,
+			}),
+		check("name", "You must supply a name").notEmpty().trim().escape(),
+		check("password", "Password must be at least 8 characters")
+			.notEmpty()
+			.isLength({
+				min: 8,
+			}),
+		check("passwordConfirm", "Confirmed password cannot be blank")
+			.notEmpty()
+			.isLength({ min: 8 }),
+	],
+	(req, res) => {
+		check("passwordConfirm", "Your passwords do not match").equals(
+			req.body.password
+		);
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			req.flash(
+				"error",
+				errors.mapped((err) => err.msg)
+			);
+			res.send({
+				title: "Register",
+				body: req.body,
+				flashes: req.flash(),
+			});
+			return;
+		}
+		User.create({
+			email: req.body.email,
+			name: req.body.name,
+		}).then((user) => res.json(user));
+	}
+);
 
 module.exports = router;
