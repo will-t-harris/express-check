98c99bdac6cd6d8f24fd5bfb4ae644ffa1d40d6c
Will Harris <will.t.harris@protonmail.com>
2020-07-15 17:31:51 -0700

    feat: add placeholder login component

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,14 +1,20 @@
 import React from "react";
+import axios from "axios";
 
-export const Login = () => {
+import { Header } from "./Header";
+
+export const Login = (props) => {
 	return (
-		<form action="/login" method="POST">
-			<h2>Login</h2>
-			<label htmlFor="email">Email Address</label>
-			<input type="email" name="email" className="bg-purple-100" />
-			<label htmlFor="password">Password</label>
-			<input type="password" name="password" className="bg-purple-100" />
-			<input type="submit" value="Log In &#10148;" />
-		</form>
+		<>
+			<Header headerText="Pony Express" />
+			<form className="pt-16" action="/login" method="POST">
+				<h2>Login</h2>
+				<label htmlFor="email">Email Address</label>
+				<input type="email" name="email" className="bg-purple-100" />
+				<label htmlFor="password">Password</label>
+				<input type="password" name="password" className="bg-purple-100" />
+				<input type="submit" value="Log In &#10148;" />
+			</form>
+		</>
 	);
 };
