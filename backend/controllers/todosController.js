b6f7f8bbd587d3bf7148792c135c5bd9d7552854
Will Harris <will.t.harris@protonmail.com>
2020-07-15 18:12:50 -0700

    refactor: move remaining methods into controller

--- backend/controllers/todosController.js
+++ backend/controllers/todosController.js
@@ -1,8 +1,51 @@
 const mongoose = require("mongoose");
 const Todo = require("../models/Todo");
+
 exports.getTodos = async (req, res) => {
 	Todo.find()
 		.then((todos) => todos.sort((a, b) => a.todoDate - b.todoDate))
 		.then((todos) => res.json(todos))
 		.catch((err) => res.status(400).json(`Error: ${err}`));
+};
+
+exports.addTodos = (req, res) => {
+	const todoContent = req.body.todoContent;
+	const todoPriority = Number(req.body.todoPriority);
+	const todoDate = Date.parse(req.body.todoDate);
+
+	const newTodo = new Todo({
+		todoContent,
+		todoPriority,
+		todoDate,
+	});
+
+	newTodo
+		.save()
+		.then(() => res.json("Todo added"))
+		.catch((err) => res.status(400).json(`Error: ${err}`));
+};
+
+exports.getTodoById = async (req, res) => {
+	Todo.findById(req.params.id)
+		.then((todo) => res.json(todo))
+		.catch((err) => res.status(400).json(`Error: ${err}`));
+};
+
+exports.deleteTodoById = (req, res) => {
+	Todo.findByIdAndDelete(req.params.id)
+		.then(() => res.json("Todo Deleted"))
+		.catch((err) => res.status(400).json(`Error: ${err}`));
+};
+
+exports.editTodoById = (req, res) => {
+	Todo.findById(req.params.id).then((todo) => {
+		todo.todoContent = req.body.todoContent;
+		todo.todoPriority = Number(req.body.todoPriority);
+		todo.todoDate = Date.parse(req.body.todoDate);
+
+		todo
+			.save()
+			.then(() => res.json("Todo updated"))
+			.catch((err) => res.status(400).json(`Error: ${err}`));
+	});
 };
--- backend/routes/todos.js
+++ backend/routes/todos.js
@@ -4,46 +4,12 @@ const todosController = require("../controllers/todosController");
 
 router.get("/", todosController.getTodos);
 
-router.route("/add").post((req, res) => {
-	const todoContent = req.body.todoContent;
-	const todoPriority = Number(req.body.todoPriority);
-	const todoDate = Date.parse(req.body.todoDate);
+router.post("/add", todosController.addTodos);
 
-	const newTodo = new Todo({
-		todoContent,
-		todoPriority,
-		todoDate,
-	});
+router.get("/:id", todosController.getTodoById);
 
-	newTodo
-		.save()
-		.then(() => res.json("Todo added"))
-		.catch((err) => res.status(400).json(`Error: ${err}`));
-});
+router.delete("/:id", todosController.deleteTodoById);
 
-router.route("/:id").get((req, res) => {
-	Todo.findById(req.params.id)
-		.then((todo) => res.json(todo))
-		.catch((err) => res.status(400).json(`Error: ${err}`));
-});
-
-router.route("/:id").delete((req, res) => {
-	Todo.findByIdAndDelete(req.params.id)
-		.then(() => res.json("Todo Deleted"))
-		.catch((err) => res.status(400).json(`Error: ${err}`));
-});
-
-router.route("/edit/:id").post((req, res) => {
-	Todo.findById(req.params.id).then((todo) => {
-		todo.todoContent = req.body.todoContent;
-		todo.todoPriority = Number(req.body.todoPriority);
-		todo.todoDate = Date.parse(req.body.todoDate);
-
-		todo
-			.save()
-			.then(() => res.json("Todo updated"))
-			.catch((err) => res.status(400).json(`Error: ${err}`));
-	});
-});
+router.post("/edit/:id", todosController.editTodoById);
 
 module.exports = router;
