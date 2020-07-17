276831a95b347b3387ddd884d24e62555e86b959
Will Harris <will.t.harris@protonmail.com>
2020-07-16 20:22:31 -0700

    feat: add password to user model

--- backend/models/User.js
+++ backend/models/User.js
@@ -19,6 +19,10 @@ const userSchema = new Schema({
 		required: "Please supply a name",
 		trim: true,
 	},
+	password: {
+		type: String,
+		required: "Password is required",
+	},
 });
 
 userSchema.plugin(passportLocalMongoose, { usernameField: "email" });
