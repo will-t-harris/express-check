cea3930c767baadd0d5ca6740784b3bfd49c59f7
Will Harris <will.t.harris@protonmail.com>
2020-07-18 13:09:03 -0700

    chore: add bodyParser to app

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ const session = require("express-session");
 const mongoose = require("mongoose");
 const MongoStore = require("connect-mongo")(session);
 const path = require("path");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const flash = require("connect-flash");
 const passport = require("passport");
@@ -22,6 +24,9 @@ app.use(cors());
 app.use(express.json());
 
 app.use(express.static(path.join(__dirname, "public")));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(
 	session({
