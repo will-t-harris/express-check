b17753de69dcef4cd7c8a2b1cecdc0ceac655cdd
Will Harris <will.t.harris@protonmail.com>
2020-07-18 13:09:10 -0700

    feat: add logout route

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -2,7 +2,12 @@ const passport = require("passport");
 
 exports.login = passport.authenticate("local", {
 	failureRedirect: "/login",
-	failureFlash: "Failed Login",
+	failureMessage: "Failed Login",
 	successRedirect: "/todos",
-	successFlash: "Login successful",
+	successMessage: "Login successful",
 });
+
+exports.logout = (req, res) => {
+	req.logout();
+	res.redirect("/todos");
+};
