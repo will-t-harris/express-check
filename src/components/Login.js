19b7ba932a60917dfb2c810cfa7a8f56eabce676
Will Harris <will.t.harris@protonmail.com>
2020-07-18 11:14:04 -0700

    feat: control form & add submit handler

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,18 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 
 import { Header } from "./Header";
 
 export const Login = (props) => {
+	const [user, setUser] = useState({ email: "", password: "" });
+
+	const onSubmit = (event) => {
+		event.preventDefault();
+
+		axios.post("http://localhost:5000/users/login", {
+			email: user.email,
+			password: user.password,
+		});
+	};
 	return (
 		<>
 			<Header headerText="Pony Express" />
-			<form className="pt-16" action="/login" method="POST">
+			<form
+				className="pt-16 flex flex-col"
+				action="/login"
+				method="POST"
+				onSubmit={onSubmit}
+			>
 				<h2>Login</h2>
 				<label htmlFor="email">Email Address</label>
-				<input type="email" name="email" className="bg-purple-100" />
+				<input
+					type="email"
+					name="email"
+					className="bg-purple-100"
+					onChange={(event) => setUser({ ...user, email: event.target.value })}
+				/>
 				<label htmlFor="password">Password</label>
-				<input type="password" name="password" className="bg-purple-100" />
+				<input
+					type="password"
+					name="password"
+					className="bg-purple-100"
+					onChange={(event) =>
+						setUser({ ...user, password: event.target.value })
+					}
+				/>
 				<input type="submit" value="Log In &#10148;" />
 			</form>
 		</>
