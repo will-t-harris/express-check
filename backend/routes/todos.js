4c45726ae98d6df2022c5f2578e83000114a2ba1
Will Harris <will.t.harris@protonmail.com>
2020-07-23 11:16:43 -0700

    feat: try using isLoggedIn

--- backend/routes/todos.js
+++ backend/routes/todos.js
@@ -1,14 +1,17 @@
 const router = require("express").Router();
 const todosController = require("../controllers/todosController");
+const authController = require("../controllers/authController");
 
 router.get("/", todosController.getAllTodos);
 
-router.post("/add", todosController.addTodos);
+router.get("/add", authController.isLoggedIn, todosController.addTodos);
 
 router.get("/:id", todosController.getTodoById);
 
 router.delete("/:id", todosController.deleteTodoById);
 
 router.post("/edit/:id", todosController.editTodoById);
+
+router.get("/isLoggedIn", authController.isLoggedIn);
 
 module.exports = router;
