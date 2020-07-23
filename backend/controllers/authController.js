f477fd07b4038956542d88f3b18201f9b2231008
Will Harris <will.t.harris@protonmail.com>
2020-07-23 11:16:25 -0700

    feat: non-functioning isLoggedIn method

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,13 +1,23 @@
 const passport = require("passport");
 
 exports.login = passport.authenticate("local", {
-	failureRedirect: "/login",
-	failureMessage: "Failed Login",
 	successRedirect: "/todos",
-	successMessage: "Login successful",
+	failureRedirect: "/login",
 });
 
 exports.logout = (req, res) => {
 	req.logout();
+	console.log("logged out");
 	res.redirect("/todos");
+};
+
+exports.isLoggedIn = (req, res, next) => {
+	// console.log(`Request: ${req}`);
+	console.log(`Response: ${res}`);
+	if (req.isAuthenticated()) {
+		console.log("authenticated");
+		return next();
+	}
+	console.log("error");
+	res.redirect("/login");
 };
