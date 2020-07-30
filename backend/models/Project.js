9605ad7beac1324e7b4a8ce9556c2d09edf648f5
Will Harris <will.t.harris@protonmail.com>
2020-07-30 16:47:55 -0700

    feat: add Project model

---
+++ backend/models/Project.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const projectSchema = new Schema({
+	projectTitle: String,
+	todos: [{ type: mongoose.Schema.Types.ObjectId, ref: "Todo" }],
+});
+
+const Project = mongoose.model("Project", projectSchema);
+
+module.exports = Project;
