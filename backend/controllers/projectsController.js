750c32f222fe3ace3916c21598a6a9aec1e711f8
Will Harris <will.t.harris@protonmail.com>
2020-07-31 12:25:20 -0700

    feat: add project with just a title

--- backend/controllers/projectsController.js
+++ backend/controllers/projectsController.js
@@ -1,8 +1,22 @@
 const mongoose = require("mongoose");
 const Project = mongoose.model("Project");
+const Todo = mongoose.model("Todo");
 
 exports.getAllProjects = (req, res) => {
 	Project.find()
 		.then((projects) => res.json(projects))
 		.catch((err) => res.status(400).json(`Error: ${err}`));
+};
+
+exports.addProject = (req, res) => {
+	const projectTitle = req.body.projectTitle;
+
+	const newProject = new Project({
+		projectTitle,
+	});
+
+	newProject
+		.save()
+		.then(() => res.json("Project added"))
+		.catch((err) => `Error: ${err}`);
 };
