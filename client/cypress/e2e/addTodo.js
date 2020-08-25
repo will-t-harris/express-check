ae35a62b8669ef7d4b9eb1bd7b3569e206f4772f
Will Harris <will.t.harris@protonmail.com>
2020-08-25 11:20:34 -0700

    test: update test

--- client/cypress/e2e/addTodo.js
+++ client/cypress/e2e/addTodo.js
@@ -1,5 +1,8 @@
 describe("add todo form", () => {
-	it("can add and delete a todo", () => {
+	it("can add a todo", () => {
+		cy.visit("/").then(() => {
+			cy.findByTestId(/todo-list/i);
+		});
 		cy.visit("/add");
 		cy.findByLabelText(/Todo Content:/i)
 			.click()
@@ -12,9 +15,15 @@ describe("add todo form", () => {
 			.get(".react-datepicker__day--010")
 			.click();
 
-		cy.findByDisplayValue(/Add Todo/i)
-			.click()
-			.url()
-			.should("eq", "http://localhost:3000/");
+		cy.findByDisplayValue(/Add Todo/i).click();
+
+		cy.url().should("eq", "http://localhost:3000/");
+	});
+
+	it("can delete a todo", () => {
+		cy.visit("/").then(() => {
+			const todos = cy.findByTestId(/todo-list/i).children();
+			console.log(todos);
+		});
 	});
 });
